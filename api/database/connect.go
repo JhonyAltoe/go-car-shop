9a7e45e5893b121b39981ef1caca223485082293
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-10-20 14:23:43 -0300

    reafactor: refatora o connect de acordo com a documentação

--- api/database/connect.go
+++ api/database/connect.go
@@ -3,29 +3,37 @@ package connect
 import (
 	"context"
 	"log"
-	"time"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func NewCollection(databaseName, collectionName string) *mongo.Collection {
-	/*
-		Connect to my cluster
-	*/
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:password@localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
-	defer client.Disconnect(ctx)
-	/*
-		List databases
-	*/
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
 	collection := client.Database(databaseName).Collection(collectionName)
+	// var entitieTcar = entities.TCar{
+	// 	Model:    "gol",
+	// 	Year:     2015,
+	// 	Color:    "blue",
+	// 	Status:   true,
+	// 	BuyValue: 20000,
+	// 	DoorsQty: 4,
+	// 	SeatsQty: 5,
+	// }
+
+	// collection.InsertOne(context.TODO(), entitieTcar)
 	return collection
 }
