c37563cc75c048f5239d81604728c9566d34a138
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-10-20 14:01:16 -0300

    feat: imprime os erros no console
    Adiciona código para que os erros sejam impressos no console

--- api/database/models/car/get-all.go
+++ api/database/models/car/get-all.go
@@ -2,6 +2,7 @@ package car_model
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jhony/go-car-shop/api/database/entities"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -11,10 +12,12 @@ func (c *carModel) GetAll(ctx context.Context) ([]entities.TCar, error) {
 	var cars []entities.TCar
 	cur, err := c.collection.Find(ctx, primitive.M{})
 	if err != nil {
+		fmt.Println(err.Error())
 		return nil, err
 	}
 	err = cur.All(ctx, &cars)
 	if err != nil {
+		fmt.Println(err.Error())
 		return nil, err
 	}
 	return cars, nil
